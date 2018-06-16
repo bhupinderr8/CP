1d57c1039c2fc6819dab8392c0285058e06b75dc
bhupinderr8 <bhupinder37kumar@gmail.com>
2018-06-16 21:14:52 +0530

    Hex created

---
+++ TopCoder/Hex.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Hex{
+    public:
+        void picture(int n, vector<string> marks){
+            cout<<marks[0];
+
+
+
+
+
+
+
+        }
+};
+
+int main(){
+
+    return 0;
+}
